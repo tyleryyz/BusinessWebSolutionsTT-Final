2d08a423b51a9f2cf10215e94d5735ad39480b37
Jesus Garcia <garciajj730@gmail.com>
2018-01-29 16:12:53 -0700

    Fixed a bug on the first video

--- client/app/components/VideoPlayer/VideoPlayer.js
+++ client/app/components/VideoPlayer/VideoPlayer.js
@@ -13,16 +13,26 @@ const VideoPlayer = () => (
     <Player playsInline
       fluid={false}
       poster="/assets/img/poster.png"
-      src="https://s3-us-west-1.amazonaws.com/jjg297-my-first-bucket/Images/small.mp4"
+      src="https://s3-us-west-1.amazonaws.com/jjg297-my-first-bucket/Videos/small.mp4"
       width={480}
       height={300}
     ><BigPlayButton position="center" /></Player>
+
+    <a href="/assets/small.mp4" download="small.mp4">
+    <button>Download Video</button>
+    </a>
+
     <Player playsInline
       fluid={false}
       src="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4"
       width={480}
       height={300}
     ><BigPlayButton position="center" /></Player>
+
+    <a href="http://clips.vorwaerts-gmbh.de/VfE_html5.mp4" download="BigBunny.mp4">
+    <button>Download Video</button>
+    </a>
+
     <Player playsInline
       fluid={false}
       poster="https://s3-us-west-1.amazonaws.com/jjg297-my-first-bucket/Images/poster2.PNG"
@@ -30,6 +40,10 @@ const VideoPlayer = () => (
       width={480}
       height={300}
     ><BigPlayButton position="center" /></Player>
+
+    <a href="https://s3-us-west-1.amazonaws.com/jjg297-my-first-bucket/Videos/SampleVideo_1280x720_1mb.mp4" download="ForbiddenDownloadbutWhy.mp4">
+    <button>Download Video</button>
+    </a>
 </div>
 );
 
