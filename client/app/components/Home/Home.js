56598ce4e13f449653ea549820871c65b70b46f5
tyleryyz <tjm375@nau.edu>
2018-02-12 08:01:43 -0700

    added mongo data retrieval

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -1,22 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'whatwg-fetch';
 var firebase = require('firebase');
-
 
 class Home extends Component {
   constructor(props) {
     super(props);
-}
+    this.state = {
+      user: null,
+      loaded: false
+    };
+    this.getData = this.getData.bind(this)
+  }
+
+  getData() {
+    let uID = this.props.user.uid;
+    console.log(uID)
+    return (fetch(`/api/users?uID=${uID}`, {
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      method: 'GET'
+    }).then(res => res.json()));
+  }
+
+  componentWillMount() {
+    let result = this.getData().then((user) => {
+      console.log("will mount here", user)
+      this.setState({
+        user: user,
+        loaded: false
+      }, () => {
+        this.setState({loaded: true})
+      });
+    })
+  };
 
   render() {
 
-    return (
-      <div>
-			we made it fam desu
-      </div>
-    );
+    if (this.state.user) {
+      return (<div>
+        {console.log("here!", this.state.user)}
+        {this.state.user.fname}
+        <p>we made it fam desu</p>
+      </div>);
+    } else {
+      return (<div>
+        <h1>Please wait</h1>
+      </div>)
+    }
   }
 }
-
 
 export default Home;
