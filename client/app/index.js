c8140e8f6711f11defd2b6881c97039c4049fff4
tyleryyz <tjm375@nau.edu>
2018-02-12 08:03:53 -0700

    removed stuff

--- client/app/index.js
+++ client/app/index.js
@@ -18,15 +18,6 @@ import './styles/styles.scss';
 
 var firebase = require('firebase');
 
-var fbconfig = {
-  apiKey: "AIzaSyDVVLR1UvlHbQtnSwPWqdbt_t3zZBwG3I8",
-  authDomain: "businesswebsolutionstt-final.firebaseapp.com",
-  databaseURL: "https://businesswebsolutionstt-final.firebaseio.com",
-  projectId: "businesswebsolutionstt-final",
-  storageBucket: "",
-  messagingSenderId: "103955267073"
-};
-
 firebase.initializeApp(fbconfig);
 
 class Index extends Component {
