99f9afd2b6fc93ffb5417915342ef6caaaf193d2
tyleryyz <tjm375@nau.edu>
2018-03-03 18:16:42 -0700

    retrieved download links for images

--- client/app/components/Claims/Claims.js
+++ client/app/components/Claims/Claims.js
@@ -3,6 +3,50 @@ import 'whatwg-fetch';
 import {Link} from 'react-router-dom';
 
 var firebase = require('firebase');
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+
+var accessKey;
+var secretAccess;
+var regionArea;
+
+fetchTextFile('http://localhost:8080/keys.txt', function(data) {
+  updateVars(data)
+});
+
+function fetchTextFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.open('GET', path, false);
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var words = httpRequest.responseText.split('\n');
+        accessKey = words[0];
+        secretAccess = words[1];
+        regionArea = words[2];
+        callback(words);
+      }
+    }
+  };
+  httpRequest.send();
+}
+
+function updateVars(data) {
+  accessKey = data[0];
+  secretAccess = data[1];
+  regionArea = data[2];
+}
+
+// Update the Access Keys
+AWS.config.update({accessKeyId: accessKey.trim(), secretAccessKey: secretAccess.trim(), region: regionArea.trim()});
+
+// Create an S3 client
+var s3 = new AWS.S3();
+var ses = new AWS.SES();
+
+var bucketName = 'jjg297-my-first-bucket';
+let file;
+var filename;
 
 // Will render a profile image, user name, user class list, user school,
 class Claims extends Component {
@@ -13,13 +57,15 @@ class Claims extends Component {
       loaded: false,
       images: null,
       courses: null,
-      filterVal: 'select'
+      filterVal: 'select',
+      downloadURL: null
     };
     this.getData = this.getData.bind(this);
     this.getImageData = this.getImageData.bind(this);
     this.submitVideo = this.submitVideo.bind(this);
     this.filterClaims = this.filterClaims.bind(this);
     this.compare = this.compare.bind(this);
+    this.getImageURL = this.getImageURL.bind(this);
   }
 
   getData() {
@@ -50,6 +96,15 @@ class Claims extends Component {
         }, () => {
           this.setState({loaded: true})
         })
+        this.getImageURL(images).then((urlArray) => {
+          console.log("after get image?", urlArray)
+          this.setState({
+            downloadURL: urlArray,
+            loaded: false
+          }, () => {
+            this.setState({loaded: true})
+          })
+        })
       });
     }).then(() => {
       this.getCourses().then((courses) => {
@@ -64,8 +119,8 @@ class Claims extends Component {
     })
   };
 
-  compare(a,b) {
-    if (a.timestamp < b.timestamp){
+  compare(a, b) {
+    if (a.timestamp < b.timestamp) {
       return -1;
     }
     if (a.timestamp > b.timestamp) {
@@ -94,26 +149,44 @@ class Claims extends Component {
     }).then(res => res.json()));
   }
 
-  getImage(imageURL) {
-    //   var getParams = {
-    //   Bucket: 'abc',
-    //   Key: imageURL
-    // }
-    //
-    // s3.getObject(getParams, function(err, data) {
-    //   if (err)
-    //       return err;
-    //     });
+  async getImageURL(images) {
+    let urlArray = new Array();
+    let downloadURL;
+    let url;
+    console.log("images", images)
+    images.map((image, index) => {
+      var params = {
+        Bucket: bucketName,
+        Key: image.imageURL
+      };
+
+    url = s3.getSignedUrl('getObject', params)
+    console.log(url)
+
+      urlArray.push(url)
+
+
+    })
+  return urlArray
+
   }
 
-  getDateInformation(timestamp){
+  getDateInformation(timestamp) {
     let date = new Date(timestamp);
     var monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
-  ];
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
 
     var day = date.getDate();
     var monthIndex = date.getMonth();
@@ -126,7 +199,7 @@ class Claims extends Component {
     e.preventDefault();
     console.log("inside filter")
     const course = e.target.value;
-    if (course === "select"){
+    if (course === "select") {
       this.getImageData().then((images) => {
         images.sort(this.compare);
 
@@ -139,25 +212,25 @@ class Claims extends Component {
         })
       });
     } else {
-    console.log(course)
-    return (fetch(`/api/images?course=${course}&tutorUID=${this.state.user.uID}`, {
-      headers: {
-        "Content-Type": "Application/json"
-      },
-      method: 'GET'
-    }).then(res => res.json()).then((images) => {
-      images.sort(this.compare);
+      console.log(course)
+      return (fetch(`/api/images?course=${course}&tutorUID=${this.state.user.uID}`, {
+        headers: {
+          "Content-Type": "Application/json"
+        },
+        method: 'GET'
+      }).then(res => res.json()).then((images) => {
+        images.sort(this.compare);
 
-      console.log(images);
-      this.setState({
-        loaded: false,
-        images: images,
-        filterVal: course
-      }, () => {
-        this.setState({loaded: true})
-      })
-    }));
-  }
+        console.log(images);
+        this.setState({
+          loaded: false,
+          images: images,
+          filterVal: course
+        }, () => {
+          this.setState({loaded: true})
+        })
+      }));
+    }
   }
 
   submitVideo() {}
@@ -165,7 +238,7 @@ class Claims extends Component {
   render() {
 
     if (this.state.user && this.state.loaded) {
-      if (this.state.user.permission === "Tutor" && this.state.images && this.state.courses) {
+      if (this.state.user.permission === "Tutor" && this.state.images && this.state.courses && this.state.downloadURL) {
         let $image;
         let $date;
         return (<div className="container">
@@ -180,18 +253,25 @@ class Claims extends Component {
           </div>
 
           {
-            this.state.images.map((image, index) => (
-              <div key={index}>
+            this.state.images.map((image, index) => (<div key={index}>
               <form onClick={this.submitVideo(image)}>
-              <p>{$image = this.getImage(image.imageURL)}</p>
+                {console.log("renderIMage", this.state.downloadURL[index])}
+                <div className="card">
+                  <a href={this.state.downloadURL[index]} download>click here to download image</a>
 
-              <p>{$date = this.getDateInformation(image.timestamp)}</p>
-
-              <p>{image.clientUID}</p>
-              <button className="button is-success">submit video</button>
-            </form>
-          <br />
-        </div>))
+                  <div className="card-content"></div>
+                  <div className="media-content">
+                    <p className="title is-4">{image.clientUID}</p>
+                    <p className="subtitle is-6">{image.course}</p>
+                  </div>
+                  <div className="content">
+                    {$date = this.getDateInformation(image.timestamp)}
+                  </div>
+                  <button className="button is-success">submit video</button>
+                </div>
+              </form>
+              <br/>
+            </div>))
           }
         </div>)
       } else if (!this.state.user.permission === "Tutor") {
