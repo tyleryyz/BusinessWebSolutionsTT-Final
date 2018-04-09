b962a86532599618b3a165ea5f0d60aef8e6a57d
tyleryyz <tjm375@nau.edu>
2018-04-09 16:53:58 -0700

    local image display

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import 'whatwg-fetch';
 import {Link} from 'react-router-dom';
+const testimage = require("../../../public/assets/img/poster.png")
 
 var firebase = require('firebase');
 
@@ -43,16 +44,75 @@ class Home extends Component {
 
     if (this.state.user && this.state.loaded) {
 
-      return (<div id="editProfileButton">
-        <div className="control">
-          <Link to="/EditProfile">Edit Profile</Link>
+      return (
+        <div>
+        <section className="headerSection">
+          <div style={{ textAlign: "center"}} className="block">
+            <img src={testimage} />
+            <h1 className="title">Tailored Tutoring Co.</h1>
+            <h2 className="subtitle">roblokken@tailoredtutoringco.com</h2>
+          </div>
+        </section>
+
+        <div className="block">
+          <section className="hero is-primary">
+            <div className="hero-body">
+
+                <div className="columns">
+                  <div className="column is-1">
+                    <a href="">
+                      <h2 className="subtitle">Image Here</h2>
+                    </a>
+                  </div>
+
+
+                  <div className="column is-2">
+
+                    <h2 style= {{fontSize: "22px" }} className="subtitle">
+                    {this.state.user.fname}{" "}{this.state.user.lname}</h2>
+                    <p className="heading">{this.state.user.school.name}</p>
+                    {this.state.user.courses.map((subject, index) => (
+                    <p key={index}>{subject}</p>
+                    ))}
+
+                    <button className="button">
+                      <Link to="/EditProfile">Edit Profile</Link>
+                    </button>
+
+                  </div>
+                </div> {/* close columns */}
+
+            </div>
+          </section>
         </div>
-        <p>{this.state.user.fname}</p>
-        <p>{this.state.user.school.name}</p>
-        <ul>
-        {this.state.user.courses.map((course, index) => (<li key={index}>{course}</li>))}
-        </ul>
-      </div>)
+
+        <div className="block">
+          <section className="subjectSection">
+              <div className="container">
+                <h1 className="title">My Subjects</h1>
+                <h2 className="subtitle">
+                  <div className="columns">
+                  {/* My For-Loop essentially */}
+                    {this.state.user.courses.map((subject, index) => (
+                        <div key={index} className="column">
+                          <a href="" style={{fontSize: "24px"}} >{subject}</a>
+                          <button className="button">
+                            <Link to="">Submit Assignment</Link>
+                          </button>
+                          <button className="button">
+                            <Link to="">View Past Submissions</Link>
+                          </button>
+                        </div>
+                    ))}
+                  </div>
+                </h2>
+              </div> {/* close container, adds a margin */}
+          </section>
+        </div>
+      </div> //close container
+
+
+      )
     } else {
       return (<div>
         <h1>Please wait</h1>
@@ -61,5 +121,14 @@ class Home extends Component {
 
   }
 }
+// <div id="editProfileButton">
+// <div className="control">
+//   <Link to="/EditProfile">Edit Profile</Link>
+// </div>
+// <p>{this.state.user.fname}</p>
+// <p>{this.state.user.school.name}</p>
+// <ul>
+// {this.state.user.courses.map((course, index) => (<li key={index}>{course}</li>))}
+// </ul>
 
 export default Home;
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -70,6 +70,10 @@ module.exports = {
             'sass-loader'
           ]
         })
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
       }
     ]
   },
