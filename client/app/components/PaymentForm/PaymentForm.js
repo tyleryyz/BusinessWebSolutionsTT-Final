7e255ca76c728bae54d8ddb063a33099b653ab59
KahnAlexander <kahn.l.alexander@gmail.com>
2018-04-09 17:16:32 -0700

    Cleaned up

--- client/app/components/PaymentForm/PaymentForm.js
+++ client/app/components/PaymentForm/PaymentForm.js
@@ -1,53 +1,3 @@
-// import React, {Component} from 'react';
-// import 'whatwg-fetch';
-//
-// var firebase = require('firebase');
-//
-// // Will render a profile image, user name, user class list, user school,
-// class Payment extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//
-//   componentDidMount () {
-//         var style = {
-//           base: {
-//             // Add your base input styles here. For example:
-//             fontSize: '16px',
-//             color: "#32325d",
-//           }
-//         };
-//
-//         // Create an instance of the card Element.
-//         var card = elements.create('card', {style: style});
-//
-//         // Add an instance of the card Element into the `card-element` <div>.
-//         card.mount('#card-element');
-//     }
-//
-//   render() {
-//       return (
-//           <form action="/charge" method="post" id="payment-form">
-//               <div class="form-row">
-//                 <label for="card-element">
-//                   Credit or debit card
-//                 </label>
-//                 <div id="card-element">
-//
-//                 </div>
-//
-//
-//                 <div id="card-errors" role="alert"></div>
-//               </div>
-//
-//               <button>Submit Payment</button>
-//             </form>
-//       );
-//   }
-// }
-//
-// export default Payment;
-
 var React = require('react');
 var ReactScriptLoaderMixin = require('react-script-loader').ReactScriptLoaderMixin;
 
@@ -113,15 +63,6 @@ var PaymentForm = React.createClass({
     }
     else {
       return (
-	// <form onSubmit={this.onSubmit} >
-    //     <span>{ this.state.paymentError }</span><br />
-    //     <input type='text' data-stripe='number' placeholder='credit card number' /><br />
-    //     <input type='text' data-stripe='exp-month' placeholder='expiration month' /><br />
-    //     <input type='text' data-stripe='exp-year' placeholder='expiration year' /><br />
-    //     <input type='text' data-stripe='cvc' placeholder='cvc' /><br />
-    //     <input disabled={this.state.submitDisabled} type='submit' value='Purchase' />
-    //   </form>);
-
 	  <div className="box">
 		<form onSubmit={this.onSubmit}>
 		<span>{ this.state.paymentError }</span><br />
