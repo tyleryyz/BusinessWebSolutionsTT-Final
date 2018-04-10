15116e11a021e4d217ee6bc83d1458a52771b44d
tyleryyz <tjm375@nau.edu>
2018-04-10 14:31:19 -0700

    signup fixes and changes

--- client/app/components/SignUp/SignUp.js
+++ client/app/components/SignUp/SignUp.js
@@ -11,13 +11,13 @@ class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      schools: null,
+      schools: [],
       selectedSchool: 'select',
       selectedCourses: [],
       loaded: false,
-      fname: null,
-      lname: null,
-      email: null,
+      fname: '',
+      lname: '',
+      email: '',
       password: null,
       nextPage: false
     }
@@ -28,7 +28,12 @@ class SignUp extends Component {
     this.coursesDeselect = this.coursesDeselect.bind(this);
     this.handleFormData = this.handleFormData.bind(this);
     this.handleBack = this.handleBack.bind(this);
+    this.updateFname = this.updateFname.bind(this);
+    this.updateLname = this.updateLname.bind(this);
+    this.updateEmail = this.updateEmail.bind(this)
+    this.addOneClass = this.addOneClass.bind(this);
   }
+
 
   componentWillMount() {
     this.getSchools().then((schools) => {
@@ -42,13 +47,18 @@ class SignUp extends Component {
     });
   }
 
-  schoolSelect(e) {
+  async schoolSelect(e) {
     let value = e.target.value;
-    var school = JSON.parse(value);
 
+    if (value != "select"){
+      var school = JSON.parse(value);
+    } else {
+      var school = value;
+    }
     this.setState({
       selectedSchool: school,
       courses: school.courses,
+      selectedCourses: [],
       loaded: false
     }, () => {
       this.setState({loaded: true})
@@ -122,12 +132,58 @@ class SignUp extends Component {
     const fname = e.target.elements.fname.value;
     const lname = e.target.elements.lname.value;
 
+    if (email && password.length >= 6 && fname && lname){
+
     this.setState({
       fname: fname,
       lname: lname,
       email: email,
       password: password,
       nextPage: true,
+      loaded: false
+    }, () => {
+      this.setState({loaded: true})
+    });
+  }
+  else if (!fname){
+    console.log("enter a first name")
+  }
+  else if (!lname){
+    console.log("enter an last name")
+  }
+  else if (!email){
+    console.log("enter an email")
+  }
+  else if (!password){
+    console.log("enter a password")
+  }
+  else if (!(password.length>=6)){
+    console.log("enter a password with at least 6 characters")
+  }
+  }
+
+  updateFname(e){
+    e.preventDefault();
+    this.setState({
+      fname: e.target.value
+    })
+  }
+  updateLname(e){
+    e.preventDefault();
+    this.setState({
+      lname: e.target.value
+    })
+  }
+  updateEmail(e){
+    e.preventDefault();
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  addOneClass(){
+    this.setState({
+      selectedCourses: this.state.selectedSchool.courses,
       loaded: false
     }, () => {
       this.setState({loaded: true})
@@ -143,6 +199,11 @@ class SignUp extends Component {
     const school = this.state.selectedSchool;
     const courses = this.state.selectedCourses;
 
+    if (!courses) {
+     console.log("no courses selected")
+    }
+
+    if (email && password && fname && lname && school!='select' && courses!=[]){
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
       var newUser = firebase.auth().currentUser;
       const uID = newUser.uid;
@@ -170,37 +231,56 @@ class SignUp extends Component {
       console.log(errorMessage);
     });
   }
+  }
 
   render() {
     let $courseData;
     if (this.state.schools != null && this.state.loaded === true) {
-      if (this.state.selectedSchool != "select") {
+      if (this.state.selectedSchool != "select" && this.state.courses) {
         console.log(this.state.selectedSchool)
-        $courseData = (<div >
-          <p>Select multiple classes that you are in</p>
-          <br/>
-          <div className="select is-multiple">
-            <select multiple={true} onChange={this.coursesSelect} value={this.state.selectedSchool.courses}>
-              {this.state.selectedSchool.courses.map((course, index) => (<option key={index}>{course}</option>))}
-            </select>
-          </div>
-          <div className="select is-multiple">
-            <select multiple={true} onChange={this.coursesDeselect} value={this.state.selectedCourses}>
-              {this.state.selectedCourses.map((course, index) => (<option key={index}>{course}</option>))}
-            </select>
-          </div>
-        </div>)
-      } else {
-        $courseData = (<p></p>)
-      }
+        if (this.state.courses.length === 1) {
+            $courseData = (
+              <div>
+            <p>Only one class available for that University</p>
+            <br />
+            <p>{this.state.courses}   <a onClick={this.addOneClass} className="button">Add Class</a></p>
+            <p>Your course list</p>
+            <div className="select is-multiple">
+              <select multiple size="3" onChange={this.coursesDeselect} value={this.state.selectedCourses}>
+                {this.state.selectedCourses.map((course, index) => (<option value={course} key={index}>{course}</option>))}
+              </select>
+            </div>
+
+            </div>
+          )
+        } else if(this.state.courses.length>1) {
+         $courseData = (
+           <div>
+           <p>Select multiple classes that you are in</p>
+           <br/>
+           <p>Available courses</p>
+           <div className="select is-multiple">
+             <select multiple size="3" id="courses" onChange={this.coursesSelect} value={this.state.selectedSchool.courses}>
+               {this.state.selectedSchool.courses.map((course, index) => (<option value={course} key={index}>{course}</option>))}
+             </select>
+           </div>
+           <p>Your course list</p>
+           <div className="select is-multiple">
+             <select multiple size="3" onChange={this.coursesDeselect} value={this.state.selectedCourses}>
+               {this.state.selectedCourses.map((course, index) => (<option value={course} key={index}>{course}</option>))}
+             </select>
+           </div>
+         </div>)
+       } else {$courseData = (<p></p>)}
+     }
       if (this.state.nextPage){
         return(
           <div className="container">
           <form onSubmit={this.handleSignUp}>
           <p> Tell us which school you go to</p>
-
           <div className="select">
-            <select onChange={this.schoolSelect} value={this.state.selectedSchool} name="school">
+          {console.log(this.state.selectedSchool)}
+            <select onChange={this.schoolSelect} value={JSON.stringify(this.state.selectedSchool)}>
               <option value="select">Select</option>
               {this.state.schools.map((school, index) => (<option value={JSON.stringify(school)} key={index}>{school.name}</option>))}
             </select>
@@ -212,9 +292,11 @@ class SignUp extends Component {
             <button className="button">Submit</button>
           </div>
           </form>
-          <button onClick={this.handleBack} id="cancelButton">
+          <div className="control">
+          <button className="button" onClick={this.handleBack} id="cancelButton">
               Go Back
             </button>
+            </div>
           </div>
         )
       }
@@ -224,19 +306,19 @@ class SignUp extends Component {
             <div className="field">
               <label className="label">First Name</label>
               <div className="control">
-                <input className="input" name="fname" type="text" placeholder="First Name"/>
+                <input className="input" onChange={this.updateFname} value={this.state.fname} name="fname" type="text" placeholder="First Name"/>
               </div>
             </div>
             <div className="field">
               <label className="label">Last Name</label>
               <div className="control">
-                <input className="input" name="lname" type="text" placeholder="Last Name"/>
+                <input className="input" onChange={this.updateLname} value={this.state.lname} name="lname" type="text" placeholder="Last Name"/>
               </div>
             </div>
             <div className="field">
               <label className="label">Email</label>
               <div className="control">
-                <input className="input" name="email" type="email" placeholder="Email input"/>
+                <input className="input" onChange={this.updateEmail} value={this.state.email} name="email" type="email" placeholder="Email input"/>
               </div>
             </div>
 
