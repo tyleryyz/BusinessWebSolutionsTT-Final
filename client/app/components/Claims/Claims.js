ca8c34b69b83571200225f87143b9e9b8cc7e46b
KahnAlexander <kahn.l.alexander@gmail.com>
2018-04-19 11:17:37 -0700

    Claims.js changes?

--- client/app/components/Claims/Claims.js
+++ client/app/components/Claims/Claims.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import 'whatwg-fetch';
 import {Link} from 'react-router-dom';
-import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 var firebase = require('firebase');
 var AWS = require('aws-sdk');
@@ -17,6 +16,7 @@ var lastname;
 var email;
 var message;
 var subject;
+
 
 fetchTextFile('http://localhost:8080/keys.txt', function(data) {
   updateVars(data)
@@ -57,7 +57,8 @@ var keyName;
 let file;
 var filename;
 
-function sendTheEmail() {
+function sendTheEmail()
+{
 
   const ses = new AWS.SES();
 
@@ -69,11 +70,17 @@ function sendTheEmail() {
       Body: {
         Html: {
           Charset: 'UTF-8',
-          Data: '<strong>First Name:</strong> ' + firstname + '<br><strong>Last Name:</strong> ' + lastname + '<br><strong>Email to:</strong> ' + email + '<br>Subject: ' + subject + '<br>Message: ' + message
+          Data:
+            '<strong>First Name:</strong> ' + firstname +
+            '<br><strong>Last Name:</strong> ' + lastname +
+            '<br><strong>Email to:</strong> ' + email +
+            '<br>Subject: '+ subject +
+            '<br>Message: ' + message
         },
         Text: {
           Charset: 'UTF-8',
-          Data: 'First Name: ' + firstname + '\nLast Name: ' + lastname + '\nEmail to: ' + email + '\nSubject: ' + subject + '\nMessage: ' + message
+          Data: 'First Name: ' + firstname + '\nLast Name: ' + lastname +
+            '\nEmail to: ' + email + '\nSubject: ' + subject + '\nMessage: ' + message
         }
       },
       Subject: {
@@ -86,12 +93,12 @@ function sendTheEmail() {
   };
 
   ses.sendEmail(params, (err, data) => {
-    if (err)
-      console.log(err, err.stack)
-    else
-      console.log(data)
-  });
+      if (err) console.log(err, err.stack)
+      else console.log(data)
+    }
+  );
 }
+
 
 // Will render a profile image, user name, user class list, user school,
 class Claims extends Component {
@@ -103,14 +110,7 @@ class Claims extends Component {
       images: null,
       courses: null,
       filterVal: 'select',
-      downloadURL: null,
-      reportID: null,
-      reportVal: 'select',
-      reportComment: '',
-      videoError1: false,
-      videoError2: false,
-      reportError: false,
-      commentError: false
+      downloadURL: null
     };
     this.getData = this.getData.bind(this);
     this.getImageData = this.getImageData.bind(this);
@@ -118,12 +118,6 @@ class Claims extends Component {
     this.filterClaims = this.filterClaims.bind(this);
     this.compare = this.compare.bind(this);
     this.getImageURL = this.getImageURL.bind(this);
-    this.renderReportForm = this.renderReportForm.bind(this);
-    this.reportChange = this.reportChange.bind(this);
-    this.handleReport = this.handleReport.bind(this);
-    this.reportImage = this.reportImage.bind(this);
-    this.cancelReport = this.cancelReport.bind(this);
-    this.enterComment = this.enterComment.bind(this);
   }
 
   getData() {
@@ -138,6 +132,7 @@ class Claims extends Component {
 
   componentWillMount() {
     let result = this.getData().then((user) => {
+      console.log("will mount here", user)
       this.setState({
         user: user,
         courses: user.courses,
@@ -155,6 +150,7 @@ class Claims extends Component {
           this.setState({loaded: true})
         })
         this.getImageURL(images).then((urlArray) => {
+          console.log("after get image?", urlArray)
           this.setState({
             downloadURL: urlArray,
             loaded: false
@@ -176,8 +172,10 @@ class Claims extends Component {
     return 0;
   }
 
+
   getImageData() {
-    return (fetch(`/api/images?tutorUID=${this.state.user.uID}&status=${ 'claimed'}`, {
+    console.log("here", this.state.uID)
+    return (fetch(`/api/images?tutorUID=${this.state.user.uID}`, {
       headers: {
         "Content-Type": "Application/json"
       },
@@ -189,18 +187,21 @@ class Claims extends Component {
     let urlArray = new Array();
     let downloadURL;
     let url;
+    console.log("images", images)
     images.map((image, index) => {
       var params = {
         Bucket: bucketName,
         Key: image.imageURL
       };
 
-      url = s3.getSignedUrl('getObject', params)
+    url = s3.getSignedUrl('getObject', params)
+    console.log(url)
 
       urlArray.push(url)
 
+
     })
-    return urlArray
+  return urlArray
 
   }
 
@@ -224,25 +225,19 @@ class Claims extends Component {
     var day = date.getDate();
     var monthIndex = date.getMonth();
     var year = date.getFullYear();
-    var hours = date.getHours();
-    var mins = date.getMinutes();
-    var seconds = date.getSeconds();
-    var amPm = "AM";
-    if (mins < 10) {
-      mins = "0" + mins;
-    }
-    if (seconds < 10) {
-      seconds = "0" + seconds;
-    }
-    if (hours > 12) {
-      hours = hours - 12;
-      amPm = "PM";
-    }
-    let dateInformation = (hours + ':' + mins + ':' + seconds + amPm + ', ' + day + ' ' + monthNames[monthIndex] + ' ' + year);
+	var hours = date.getHours();
+	var mins = date.getMinutes();
+	var seconds = date.getSeconds();
+	var amPm = "AM";
+	if(mins < 10) { mins = "0" + mins; }
+	if(seconds < 10) { seconds = "0" + seconds; }
+	if(hours > 12) { hours = hours-12; amPm = "PM";}
+    let dateInformation = (hours + ':' + mins + ':' + seconds + amPm +
+	', ' + day + ' ' + monthNames[monthIndex] + ' ' + year);
     return (dateInformation);
   }
 
-  async filterClaims(e) {
+  filterClaims(e) {
     let course;
     if (e) {
       e.preventDefault();
@@ -262,6 +257,7 @@ class Claims extends Component {
           this.setState({loaded: true})
         })
         this.getImageURL(images).then((urlArray) => {
+          console.log("after get image?", urlArray)
           this.setState({
             downloadURL: urlArray,
             loaded: false
@@ -271,6 +267,7 @@ class Claims extends Component {
         })
       });
     } else {
+      console.log(course)
       return (fetch(`/api/images?course=${course}&tutorUID=${this.state.user.uID}`, {
         headers: {
           "Content-Type": "Application/json"
@@ -279,6 +276,7 @@ class Claims extends Component {
       }).then(res => res.json()).then((images) => {
         images.sort(this.compare);
 
+        console.log(images);
         this.setState({
           loaded: false,
           images: images,
@@ -305,281 +303,89 @@ class Claims extends Component {
 
   submitVideo(e, image) {
 
-    e.preventDefault();
+	e.preventDefault();
 
-    email = this.state.user.email;
-    firstname = this.state.user.fname;
-    lastname = this.state.user.lname;
+	email = this.state.user.email;
+	firstname = this.state.user.fname;
+	lastname = this.state.user.lname;
 
-    if(!this.state.file){
-      this.setState({
-        videoError1: true,
-        loaded: false
-      }, () => {
-        this.setState({loaded: true})
-      });
-    }
+	console.log(firstname + " " + lastname + " " + email);
+	console.log(file.name);
 
-    else{
-      filename = this.state.file.name;
+	filename = this.state.file.name;
 
-      var d = new Date();
-      var timestamp = d.getTime();
-      var uploadName = this.props.user.uid + '-' + timestamp;
+	var d = new Date();
+	var timestamp = d.getTime();
+	var uploadName = this.props.user.uid+'-'+timestamp;
 
-      var extension = filename.split(".");
-      if (extension.length === 1 || (extension[0] === "" && extension.length === 2)) {
-        return "";
-      }
-      extension = extension.pop(); // feel free to tack .toLowerCase() here if you want
-      uploadName = uploadName + '.' + extension;
-      var keyName;
-      extension = extension.toLowerCase();
+	var extension = filename.split(".");
+	if( extension.length === 1 || ( extension[0] === "" && extension.length === 2 ) ) {
+		return "";
+	}
+	extension = extension.pop();    // feel free to tack .toLowerCase() here if you want
+	uploadName = uploadName+'.'+extension;
+	var keyName;
+  extension = extension.toLowerCase();
 
-      if (extension === "mp4" || extension === "wmv" || extension === "flv" || extension === "avi") {
-        keyName = "Videos/"
-      } else {
+	if (extension=="mp4" || extension=="wmv" || extension=="flv" || extension=="avi")
+	{
+		keyName = "Videos/"
+	} else {
+		console.log("Invalid file type!")
+	}
+
+	uploadName = keyName+uploadName;
+
+	var params = {
+	  Bucket: bucketName,
+	  Key: uploadName,
+	  Body: file
+	};
+
+	console.log(uploadName);
+
+	const imageURL = image.imageURL;
+    fetch(`/api/images?imageURL=${imageURL}`, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify({
+		  videoURL: uploadName,
+		  status: "completed" })
+    }).then((image) => {
+      this.getImageData().then((images) => {
         this.setState({
-          videoError2: true,
+          images: images,
           loaded: false
         }, () => {
           this.setState({loaded: true})
         });
-      }
-
-      if (!keyName) {
-        this.setState({
-          videoError2: true,
-          loaded: false
-        }, () => {
-          this.setState({loaded: true})
-        });
-      }
-
-      if (keyName) {
-        uploadName = keyName + uploadName;
-
-        var params = {
-          Bucket: bucketName,
-          Key: uploadName,
-          Body: file
-        };
-
-        const imageURL = image.imageURL;
-
-        s3.putObject(params, ((err, data) => {
-          if (err) {
-            console.log(err)
-          } else {
-            console.log("Successfully uploaded data to: " + bucketName + "/" + uploadName);
-
-            fetch(`/api/images?imageURL=${imageURL}`, {
-              method: 'PUT',
-              headers: {
-                "Content-Type": "Application/json"
-              },
-              body: JSON.stringify({videoURL: uploadName, status: "completed"})
-            }).then((image) => {
-              this.getImageData().then((images) => {
-
-                this.setState({
-                  images: images,
-                  loaded: false
-                }, () => {
-                  this.setState({loaded: true})
-                });
-                this.getImageURL(images).then((urlArray) => {
-                  this.setState({
-                    downloadURL: urlArray,
-                    loaded: false
-                  }, () => {
-                    this.setState({loaded: true})
-                  })
-                })
-              }).then(() => {
-                NotificationManager.success('Video was successfully uploaded!', 'Success!');
-              })
-            });
-
-            subject = "Submission Received!";
-            message = "We have received your image submission of: " + filename + "!";
-            sendTheEmail();
-          }
-        }))
-
-        console.log('Handling uploading, data presented: ', this.state.file);
-      }
-    }
-}
-
-  handleReport(image) {
-    this.setState({
-      reportID: image.imageURL,
-      reportVal: 'select',
-      loaded: false
-    }, () => {
-      this.setState({loaded: true})
-    })
-  }
-
-  reportChange(e) {
-    e.preventDefault();
-    this.setState({
-      reportVal: e.target.value,
-      loaded: false
-    }, () => {
-      this.setState({loaded: true})
-    })
-  }
-
-  renderReportForm(image) {
-    if (this.state.reportID === image.imageURL) {
-      let $otherForm;
-      if (this.state.reportVal === 'Other') {
-        $otherForm = (<div className="field">
-          <label className="label">Specify</label>
-          {
-            this.state.commentError
-              ? <p style={{
-                    color: 'red'
-                  }}>Please specify</p>
-              : <p></p>
-          }
-
-          <div className="control">
-            <input className="input" onChange={this.enterComment} value={this.state.reportComment} name="report" type="text" placeholder="Specify Report"/>
-          </div>
-        </div>)
-      } else {
-        $otherForm = (<p></p>)
-      }
-      return (<form onSubmit={(e) => this.reportImage(e, image)}>
-        {
-          this.state.reportError
-            ? <p style={{
-                  color: 'red'
-                }}>Please pick a reason</p>
-            : <p></p>
-        }
-
-        <div className="select">
-          <select onChange={this.reportChange} value={this.state.reportVal} name="report">
-            <option value="select">Select</option>
-            <option value="Inappropriate">Inappropriate Image/Comment</option>
-            <option value="Misplaced">Wrong course tag for image content</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
-        {$otherForm}
-        <div className="control">
-          <button className="button">Submit Report</button>
-        </div>
-        <div id="cancelButton">
-          <div className="control">
-            <button onClick={this.cancelReport} className="button">Cancel Report</button>
-          </div>
-        </div>
-      </form>)
-    } else {
-      return (<p></p>)
-    }
-  }
-
-  enterComment(e) {
-    e.preventDefault();
-    this.setState({reportComment: e.target.value})
-  }
-
-  cancelReport() {
-    this.setState({
-      reportVal: 'select',
-      reportID: null,
-      loaded: false
-    }, () => {
-      this.setState({loaded: true})
-    })
-  }
-
-  reportImage(e, image) {
-    e.preventDefault();
-
-    if (this.state.reportComment === "") {
-      this.setState({
-        commentError: true,
-        loaded: false
-      }, () => {
-        this.setState({loaded: true})
       })
-    }
-    if (this.state.reportVal === 'select') {
-      this.setState({
-        reportError: true,
-        loaded: false
-      }, () => {
-        this.setState({loaded: true})
-      })
-    }
-    if (this.state.reportComment != "" && this.state.reportVal != 'select') {
-      const comment = this.state.reportComment;
-      const imageURL = image.imageURL;
-      fetch(`/api/images?imageURL=${imageURL}`, {
-        method: 'PUT',
-        headers: {
-          "Content-Type": "Application/json"
-        },
-        body: JSON.stringify({status: "reported", reportComment: comment, reportReason: this.state.reportVal})
-      }).then((image) => {
-        this.filterClaims().then(() => {
-          this.getImageURL(this.state.images).then((urlArray) => {
-            this.setState({
-              downloadURL: urlArray,
-              reportID: null,
-              reportVal: 'select',
-              reportComment: '',
-              reportError: false,
-              commentError: false,
-              loaded: false
-            }, () => {
-              this.setState({loaded: true})
-            })
-          })
-        })
-      }).then(() => {
-        NotificationManager.success('Report was sent to site admin.', 'Report success');
-      })
-    } else if (this.state.reportVal != 'select') {
-      const imageURL = image.imageURL;
-      fetch(`/api/images?imageURL=${imageURL}`, {
-        method: 'PUT',
-        headers: {
-          "Content-Type": "Application/json"
-        },
-        body: JSON.stringify({status: "reported", reportReason: this.state.reportVal})
-      }).then((image) => {
-        this.filterClaims().then(() => {
-          this.getImageURL(this.state.images).then((urlArray) => {
-            this.setState({
-              downloadURL: urlArray,
-              reportID: null,
-              reportVal: 'select',
-              reportComment: '',
-              reportError: false,
-              commentError: false,
-              loaded: false
-            }, () => {
-              this.setState({loaded: true})
-            })
-          })
-        })
-      }).then(() => {
-        NotificationManager.success('Report was sent to site admin.', 'Report success');
-      })
-    }
+    });
+
+	s3.putObject(params, function(err, data) {
+	   if (err)
+	   {
+	     console.log(err)
+	   }
+	   else
+	   {
+	     console.log("Successfully uploaded data to: " + bucketName + "/" + uploadName);
+
+	     subject = "Submission Received!";
+	     message = "We have received your image submission of: "+filename+"!";
+	     sendTheEmail();
+	   }
+	 })
+
+	console.log('Handling uploading, data presented: ', this.state.file);
+
   }
 
-  // BUG: If there are more than one images with differing courses,
-  // The first image will populate the image space as opposed to the
-  // proper image that regards to that case.
+	// BUG: If there are more than one images with differing courses,
+	// The first image will populate the image space as opposed to the
+	// proper image that regards to that case.
   render() {
 
     if (this.state.user && this.state.loaded) {
@@ -587,7 +393,7 @@ class Claims extends Component {
         let $image;
         let $date;
         return (<div className="container">
-
+          <p>tutor view</p>
           <Link to="/Dashboard">Back to dashboard</Link>
           <p>Sort by course tag</p>
           <div className="select">
@@ -599,13 +405,13 @@ class Claims extends Component {
 
           {
             this.state.images.map((image, index) => (<div key={index}>
-              <div className="card">
-                <form>
-
+              <form>
+                {console.log("renderImage", this.state.downloadURL[index])}
+                <div className="card">
                   <div className="card-content">
-                    <a href={this.state.downloadURL[index]} download="download"><img src={this.state.downloadURL[index]} width="75%" height="75%"/></a><br/>
-                    <a href={this.state.downloadURL[index]} download="download">click here to download image</a>
-                  </div>
+				  <a href={this.state.downloadURL[index]} download="download"><img src={this.state.downloadURL[index]} width="75%" height="75%"/></a><br />
+                  <a href={this.state.downloadURL[index]} download>click here to download image</a>
+				  </div>
                   <div className="media-content">
                     <p className="title is-4">{image.clientUID}</p>
                     <p className="subtitle is-6">{image.course}</p>
@@ -613,33 +419,15 @@ class Claims extends Component {
                   <div className="content">
                     {$date = this.getDateInformation(image.timestamp)}
                   </div>
-                  {
-                    this.state.videoError1
-                      ? <p style={{
-                            color: 'red'
-                          }}>Please add a file</p>
-                      : <p></p>
-                  }
-                  {
-                    this.state.videoError2
-                      ? <p style={{
-                            color: 'red'
-                          }}>Please add a file with a supported type: mp4, wmv, flv, avi</p>
-                      : <p></p>
-                  }
-
-                  <input className="fileInput" type="file" onChange={(e) => this._handleFileChange(e)}/><br/>
+				  <input className="fileInput" type="file" onChange={(e) => this._handleFileChange(e)}/><br />
                   <button className="button is-success" onClick={(e) => this.submitVideo(e, image)}>submit video</button>
-                </form>
-                <button onClick={() => this.handleReport(image)} className="button is-warning">Report Image</button>
-                {this.renderReportForm(image)}
-              </div>
-
+                </div>
+              </form>
               <br/>
             </div>))
           }
         </div>)
-      } else if (!(this.state.user.permission === "Tutor")) {
+      } else if (!this.state.user.permission === "Tutor") {
         return (<div>
           <p>You do not have permission to view this page</p>
         </div>);
