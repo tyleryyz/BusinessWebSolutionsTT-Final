2219a3ceb375323b5d0149e88d6b4bb7717f91fa
KahnAlexander <kahn.l.alexander@gmail.com>
2018-04-30 12:54:51 -0700

    Got burger working on mobile

--- client/app/components/NavBar/NavBar.js
+++ client/app/components/NavBar/NavBar.js
@@ -11,6 +11,10 @@ class NavBar extends Component {
   constructor(props) {
     super(props);
     this.handleSignOut = this.handleSignOut.bind(this)
+	this.handleBurger = this.handleBurger.bind(this)
+	this.state = {
+		burgerClicked: false
+	}
   }
 
   handleSignOut() {
@@ -22,36 +26,116 @@ class NavBar extends Component {
     this.props.logout()
   }
 
+  handleBurger(e){
+	  e.preventDefault();
+	  console.log("Toggling");
+	  this.setState({
+		  burgerClicked: !this.state.burgerClicked,
+		  loaded: false
+	  }, () => {
+		  this.setState({loaded: true})
+		  console.log("Burger clicked ", this.state.burgerClicked);
+	  });
+  }
+
   render() {
     if (this.props.auth) {
       // User is signed in.
-      return (
-        <nav className="navbar" aria-label="main navigation">
-          <div className="navbar-brand">
+	  console.log("Burger clicked ", this.state.burgerClicked);
 
-		  	<img style={{height: 60}} src={ttcLogo} />
+	  if(this.state.burgerClicked)
+	  {
+		  return (
+			  <section>
+		        <nav className="navbar" role="navigation" aria-label="main navigation">
+		          <div className="navbar-brand">
 
-            <button className="button navbar-burger">
-              <span></span>
-              <span></span>
-              <span></span>
-            </button>
-          </div>
-		  <div className="navbar-menu">
-		  	<div className="navbar-start">
+				  	<img style={{height: 60}} src={ttcLogo} />
+
+		            <a className="navbar-burger burger is-active" id="burger" ref="burger" data-target="navbartabs" onClick={(e) => this.handleBurger(e)}>
+		              <span></span>
+		              <span></span>
+		              <span></span>
+		            </a>
+		          </div>
+				  <div className="navbar-start">
+
+					<div className="navbar-menu is-hidden-desktop is-active" id="navbartabs">
+						<Link className="navbar-item" to="/">
+			              Home
+			            </Link>
+			            <Link className="navbar-item" to="/dashboard">
+			              Assignments
+			            </Link>
+						<button style={{margin: 15}} className="navbar-item button" onClick={this.handleSignOut}>Sign Out</button>
+					</div>
+
+					<div className="navbar-tabs is-hidden-touch">
+						<Link className="navbar-item" to="/">
+			              Home
+			            </Link>
+			            <Link className="navbar-item" to="/dashboard">
+			              Assignments
+			            </Link>
+					</div>
+
+				  </div>
+
+				  <div className="navbar-end is-hidden-touch">
+					<button style={{margin: 15}} className="button" onClick={this.handleSignOut}>Sign Out</button>
+				  </div>
+		        </nav>
+			</section>
+			)
+	    }
+		else if(!this.state.mobileClicked)
+		{
+
+			return (
+				<section>
+				<nav className="navbar" role="navigation" aria-label="main navigation">
+				<div className="navbar-brand">
+
+				<img style={{height: 60}} src={ttcLogo} />
+
+				<a className="navbar-burger burger" id="burger" ref="burger" data-target="navbartabs" onClick={(e) => this.handleBurger(e)}>
+				<span></span>
+				<span></span>
+				<span></span>
+				</a>
+				</div>
+				<div className="navbar-start">
+
+				<div className="navbar-menu is-hidden-desktop" id="navbartabs">
 				<Link className="navbar-item" to="/">
-	              Home
-	            </Link>
-	            <Link className="navbar-item" to="/dashboard">
-	              Assignments
-	            </Link>
-			</div>
-			<div className="navbar-end">
+				Home
+				</Link>
+				<Link className="navbar-item" to="/dashboard">
+				Assignments
+				</Link>
+				<button style={{margin: 15}} className="navbar-item button" onClick={this.handleSignOut}>Sign Out</button>
+				</div>
+				<div className="navbar-tabs is-hidden-touch">
+				<Link className="navbar-item" to="/">
+				Home
+				</Link>
+				<Link className="navbar-item" to="/dashboard">
+				Assignments
+				</Link>
+				</div>
+
+				</div>
+
+				<div className="navbar-end is-hidden-touch">
 				<button style={{margin: 15}} className="button" onClick={this.handleSignOut}>Sign Out</button>
-			</div>
-		  </div>
-        </nav>)
-    } else {
+				</div>
+			</nav>
+		</section>
+	)
+	}
+	  }
+
+    else {
       return (
 		  <nav className="navbar" aria-label="main navigation">
 		  <div className="navbar-brand">
